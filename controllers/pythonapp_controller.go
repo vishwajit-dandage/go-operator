2c644a67eda6ca402f644765c7f0cdc0337ef8da
Yuvraj Vansure <yuvraj.vansure1.ibm.com>
2022-12-16 14:18:15 +0530

    deployment updated

--- controllers/pythonapp_controller.go
+++ controllers/pythonapp_controller.go
@@ -117,7 +117,6 @@ func (r *PythonAppReconciler) createDeployment(app *testv1alpha1.PythonApp) *app
 					},
 				},
 				Spec: corev1.PodSpec{
-					ServiceAccountName: "controller-manager",
 					Containers: []corev1.Container{
 						{
 							Image: image,
